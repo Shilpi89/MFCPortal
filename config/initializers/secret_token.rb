b7d37e7d05451cfc4890f0382cf30764b19445fe
Ashish Upadhyay <ashish@ubuntu.ubuntu-domain>
2012-08-16 15:35:35 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MFCPortal::Application.config.secret_token = '16b8b45fb2cfe2393408f3755ce1b26ade86467f59089315be71e9772bd04e754f82f7ee5d21470cb3d28a926f6198d951c52bdf225ddc7eba41cd45cd9233d0'
